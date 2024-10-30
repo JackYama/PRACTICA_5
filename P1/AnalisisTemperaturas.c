f9bb8b5e372b72b50ace491cbe3d364dabac72ac
daviz616 <david.castejon616@gmail.com>
2024-10-30 14:03:08 +0100

    P1

--- P1/AnalisisTemperaturas.c
+++ P1/AnalisisTemperaturas.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+//Autor:Jacob Navarro Llamazares,David Castejón García
+
+#define dias_semana 7
+
 int main(){
+
+	float temperatura[dias_semana]; 
+	char dias[dias_semana][15] = {
+		"Lunes: ", "Martes: ", "Miércoles: ","Jueves: ","Viernes: ","Sábado: ","Domingo: "
+	};
+
+	//Ingreso de datos
+	for (int i = 0; i < dias_semana; i++){
+		printf("Ingresa la temperatura promedio del %s", dias[i]);	
+		scanf("%f", &temperatura[i]);
+
+
+}
+	
+	return 0;
 }
