515b13098dbb252dcaeaab69db34e249d9879bf1
JackYama <Jackllama2006@gmail.com>
2024-11-06 11:09:55 +0100

    AnalisisNotas_V.2

--- P2/AnalisisNotas.c
+++ P2/AnalisisNotas.c
@@ -7,19 +7,22 @@
 
 //definimos la estructura del estudiante y lo que define dentro
 struct Estudiante{
-	int ID
-       	double nota	
+	int id;
+       	double nota;	
 };
-int main{
+int main(){
+	         
 	//aqui pedimos al cliente que ingrese el numero de estudiantes y con un scanf cogemos el numero que nos de el cliente
+
+
 	int num_estudiantes;
 		printf("Ingresa el numero de estudiantes:");
-		scanf("%d",&num_estudiantes)
+		scanf("%d",&num_estudiantes);
 		
 			//declaramos un array sobre los estudiantes
 			//este array lo que hara es almacenar todos los datos que introduzca el cliente para luego poder explorarlos y consultarlos para cualquier función que definamos
 
-	Estudiante estudiantes[num_estudiantes]
+	struct Estudiante estudiantes[num_estudiantes];
 	
 	//ahora lo que queremos definir es que el cliente nos ingrese la nota de cada estudiante
 	
@@ -29,14 +32,16 @@ int main{
 		printf("Ingrese la nota final del estudiante %d: ", estudiantes[i].id);
 		//y aqui lo escaneamos y almacenamos
 		scanf("%lf", &estudiantes[i].nota);
-}
+	}
 //ahora al ya tener las notas introducidas y metidas dentro de nuestro array ahora queremos definir la función de como vamos a calcular el promedio de las notas
 // Cálculo del promedio
 double suma = 0.0;  // Variable para almacenar la suma de las notas
 	for (int i = 0; i < num_estudiantes; i++) {
         suma += estudiantes[i].nota;  // Sumamos las notas de todos los estudiantes
-}
+	}
    	
 	double promedio = suma / num_estudiantes;  // Calculamos el promedio
 		printf("Promedio de notas: %.2f\n", promedio);  // Mostramos el promedio con 2 decimales
-//                                  
+	
+	return 0;
+} 
